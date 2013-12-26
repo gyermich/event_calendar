862d8aab3926bfd2750ab8bc8f2680d3d93584a7
Galina <yermichevagalina@gmail.com>
2013-12-26 09:52:52 -0800

    generated users resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Calendar::Application.routes.draw do
+  resources :users
+
 resources :events
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131223180110) do
+ActiveRecord::Schema.define(version: 20131226175154) do
 
   create_table "events", force: true do |t|
     t.string   "name"
     t.string   "url"
     t.datetime "date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
