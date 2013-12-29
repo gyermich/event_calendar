e26a62a46c9bd5e84caca09030ed60a5262cfe68
Galina <yermichevagalina@gmail.com>
2013-12-28 16:53:15 -0800

    created session controler, log in form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Calendar::Application.routes.draw do
-  resources :users
 
-resources :events
+  root 'events#index'
+  resources :sessions
+  resources :users
+  resources :events
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
