f9787a17408bb7181d13dfa7207627b0be531447
Galina <yermichevagalina@gmail.com>
2013-12-29 00:02:40 -0800

    created events_user model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,11 @@
 class Event < ActiveRecord::Base
 
+  has_many :users, through :events_users
+  has_many :events_users
+
+  validates :name, presence: true
+  validates :description, presence: true
+
   def start_time
     date
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :events, through :events_users
+  has_many :events_users
 
-  validates_uniqueness_of :email
+  validates_presence_of :name, :email
+  validates             :email, uniqueness: true 
+  validates             :name, length: { minumum: 3, maximum: 30 }
+  validates_format_of   :email, :with => /\w+@\w+\.\w+/i
 end
