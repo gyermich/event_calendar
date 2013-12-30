b5c23297c04ff9ab9430139dd2222df4c49cd69e
Galina <yermichevagalina@gmail.com>
2013-12-29 21:35:10 -0800

    created comment controller, comment form

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,12 @@
+class CommentsController < ApplicationController
+  def create
+    @event = Event.find_by_id(params[:event_id])
+    @comment = @event.comments.create(comment_params)
+    redirect_to event_path(@event)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body, :user_id, :event_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Calendar::Application.routes.draw do
 
+ 
   get "log_in"  => "sessions#new",     :as => "login"
   get "log_out" => "sessions#destroy", :as => "logout"
   get "sign_up" => "users#new",        :as => "signup"
+
+  resources :events do
+    resources :comments
+  end
 
   root 'events#index'
   resources :sessions
   resources :users
   resources :events
   resources :events_users
+  resources :comments
 
   post '/events/:id/join' => "events#join", as: :join
   delete '/events/:id/not_going' => "events#not_going", as: :not_going
