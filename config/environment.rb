105c79ec10914bf1ed025b223711e8c580b75d32
Galina <yermichevagalina@gmail.com>
2014-01-03 00:10:11 -0800

    changed production env settings

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Calendar::Application.initialize!
+
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+address:              'smtp.gmail.com',
+port:                 587,
+domain:               'mail.google.com',
+user_name:            'ada.event.calendar',
+password:             'Lovelace13',
+authentication:       'plain',
+enable_starttls_auto: true  }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,18 +19,18 @@ Calendar::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  # config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.default_url_options = {:host => 'localhost:3000'} 
-  # config.action_mailer.perform_deliveries = true
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  # address:              'smtp.gmail.com',
-  # port:                 587,
-  # domain:               'mail.google.com',
-  # user_name:            'ada.event.calendar',
-  # password:             'Lovelace13',
-  # authentication:       'plain',
-  # enable_starttls_auto: true  }
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = {:host => 'portal.herokuapp.com'} 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'mail.google.com',
+  user_name:            'ada.event.calendar',
+  password:             'Lovelace13',
+  authentication:       'plain',
+  enable_starttls_auto: true  }
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
