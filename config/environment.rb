2ec22eac54634b8323889eb253f73c8a6f9bb742
Galina <yermichevagalina@gmail.com>
2014-01-03 11:58:22 -0800

    config for sendgrid added

--- config/environment.rb
+++ config/environment.rb
@@ -5,12 +5,4 @@ require File.expand_path('../application', __FILE__)
 Calendar::Application.initialize!
 
 
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-address:              'smtp.gmail.com',
-port:                 587,
-domain:               'mail.google.com',
-user_name:            'ada.event.calendar',
-password:             'Lovelace13',
-authentication:       'plain',
-enable_starttls_auto: true  }
+
