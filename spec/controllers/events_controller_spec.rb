4eaa1460c16e714d453e229c638ec8d725e51904
Galina <yermichevagalina@gmail.com>
2014-01-05 20:47:31 -0800

    added specs

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,24 +1,24 @@
 require 'spec_helper'
 
 describe EventsController do
+  
   describe "GET #index" do
-      it "responds successfully with an HTTP 200 status code" do
-        get :index
-        expect(response).to be_success
-        expect(response.status).to eq(200)
-      end
-
-      it "renders the index template" do
-        get :index
-        expect(response).to render_template("index")
-      end
-
-      it "loads all of the events into @events" do
-        event1, event2 = Event.create!(name:"Birthday", description:"some txt", user_id:1, date:'2014-01-08T19:00'), 
-                         Event.create!(name:"OneMoreBirthday", description:"some txt", user_id:1, date:'2014-02-08T19:00')
-        get :index
-
-        expect(assigns(:events)).to match_array([event1, event2])
-      end
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response.status).to eq(200)
     end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+
+    it "loads all of the events into @events" do
+      event1, event2 = Event.create!(name:"Birthday", description:"some txt", user_id:1, date:'2014-01-08T19:00'), 
+                       Event.create!(name:"OneMoreBirthday", description:"some txt", user_id:1, date:'2014-02-08T19:00')
+      get :index
+      expect(assigns(:events)).to match_array([event1, event2])
+    end
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,16 +1,30 @@
 require 'spec_helper'
 
 describe UsersController do
-  describe "GET #index" do
-      it "responds successfully with an HTTP 200 status code" do
-        get :index
-        expect(response).to be_success
-        expect(response.status).to eq(200)
-      end
 
-      it "renders the index template" do
-        get :index
-        expect(response).to render_template("index")
-      end
+  describe "GET #index" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response.status).to eq(200)
     end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+
+
+  describe "GET 'new'" do
+    it "returns http success" do
+      get 'new'
+      response.should be_success
+    end
+
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template("new")
+    end
+  end
 end
