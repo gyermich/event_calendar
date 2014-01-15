b506dccc1473cc7331608e2161589ced7168fe1e
Galina <yermichevagalina@gmail.com>
2014-01-14 21:46:38 -0800

    added stats page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Calendar::Application.routes.draw do
 
  
-  resources :visitors
+  
 
   get "log_in"  => "sessions#new",     :as => "login"
   get "log_out" => "sessions#destroy", :as => "logout"
   get "sign_up" => "users#new",        :as => "signup"
+  get "/stats"  => "visitors#index",   :as => "stats"
 
   resources :events do
     resources :comments
@@ -17,6 +18,7 @@ Calendar::Application.routes.draw do
   resources :events
   resources :events_users
   resources :comments
+  resources :visitors
 
   post '/events/:id/join' => "events#join", as: :join
   delete '/events/:id/not_going' => "events#not_going", as: :not_going
