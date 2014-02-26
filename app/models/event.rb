b05be04f7e82d6011fc0b5095d4a54057d1977ec
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-02-25 23:39:16 -0800

    add reverse geocode for address, has bug

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ActiveRecord::Base
   geocoded_by :address
   after_validation :geocode
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :reverse_geocode
 
   has_many :users, through: :events_users
   has_many :events_users
