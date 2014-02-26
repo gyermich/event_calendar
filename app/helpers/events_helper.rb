347954c50346d63afebaff1728712da9228ae7cd
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-02-26 01:03:06 -0800

    add helper for zoom

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,22 @@
 module EventsHelper
+  def zoom
+    event = Event.find(params[:id])
+    case 
+      when event.distance < 0.4
+        zoom = 16
+      when event.distance < 0.6
+        zoom = 15
+      when event.distance < 1
+        zoom = 14
+      when event.distance < 1.5
+        zoom = 13
+      when event.distance < 3
+        zoom = 12
+      when event.distance < 6
+        zoom = 11
+      else
+        zoom = 10
+    end
+    zoom
+  end
 end
